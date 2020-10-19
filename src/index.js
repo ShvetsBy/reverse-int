f0f324a0ca26b08f664eba8b1ef5456a8e319def
iShvets <shvets.ivan@gmail.com>
2020-10-19 20:17:50 +0300

    feat: add reverse int feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function reverse (n) {
-  
+module.exports = 
+function reverse (n) {
+    let result = '';
+
+    if (n < 0) {
+
+        n = n * -1;
+        let int = n.toString();
+        let arr = Array.from(int);
+    } 
+    let int = n.toString();
+    let arr = Array.from(int).reverse();
+    
+    for (i = 0; i < arr.length; i++) {
+        result = result + arr[i];
 }
+    
+    console.log(result); 
+    return result;
+    
+
+
+}
+
+// reverse (123456789)
